8727d225d3178b20c9bdd211422ff236fdb35966
Andrew Lackey <77242875+ALackey-ai@users.noreply.github.com>
2024-02-09 19:16:57 +0000

    I think this is all that I need to do here for now

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-exit 127
+# This build script will build the project and create the docker image needed for deployment
+# Upon failure of compiling code this will return 1, upon failure of docker image build return 2, upon other failure return 3
+
+#TODO: compile and build project here such that it can be automatically run by github actions.
+
+#for now this will return 0 because nothing can be built as of yet.
+exit 0
