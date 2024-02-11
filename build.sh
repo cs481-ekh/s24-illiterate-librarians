52e6d04f2223b7b3e078e573d7dc46beb1ab055e
Andrew Lackey <77242875+ALackey-ai@users.noreply.github.com>
2024-02-11 16:00:57 +0000

    added docker build and teardown commands

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-# This build script will build the project and create the docker image needed for deployment
-# Upon failure of compiling code this will return 1, upon failure of docker image build return 2, upon other failure return 3
 
-#TODO: compile and build project here such that it can be automatically run by github actions.
+# Exit script on any error
+set -e
+
+# Build and run Docker Compose services
+docker-compose up -d
+
+# Perform other actions:
+
+
+# Clean up - stop and remove Docker Compose services
+docker-compose down
 
 #for now this will return 0 because nothing can be built as of yet.
 exit 0
