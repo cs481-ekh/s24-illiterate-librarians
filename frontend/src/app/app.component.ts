51cb19fa5ccfb32c4d55308e510239dfed08cff2
AndrewDoering01 <97553758+AndrewDoering01@users.noreply.github.com>
2024-02-15 14:46:04 -0700

    Closes #31 (#86)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
+  template: `
+    <main>
+      <app-header></app-header>
+      <router-outlet></router-outlet>
+    </main>
+  `,
+
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Boise State Literacy Lab';
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Literacy Link',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'Dashboard',
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    title: 'Profile',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'login',
+  },
+];
