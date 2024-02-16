406e49df1d9ae26fa815f245a0079596f8229669
AndrewDoering01 <andrewdoering@u.boisestate.edu>
2024-02-15 19:40:12 -0700

    added menu autoclose on resize

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,32 +1,75 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, RouterModule],
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          opacity: 1,
+          visibility: 'visible',
+          backgroundColor: 'white',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          visibility: 'hidden',
+          height: '0px',
+          opacity: 0,
+        })
+      ),
+      transition('open => closed', [animate('0.2s')]),
+      transition('closed => open', [animate('0.2s')]),
+    ]),
+  ],
   template: `
     <header class="header">
       <a href="https://sdp.boisestate.edu/index.html">
-        <img
-          src="../assets/sdp-logo-3.png"
-          alt="SDP Logo"
-          width="100px"
-          heigh="100px"
-        />
+        <img src="../assets/sdp-logo-3.png" alt="SDP Logo" width="100px" />
       </a>
-      <a [routerLink]="['/']">
-        <span class="material-symbols-outlined"> menu </span>
+      <a (click)="toggle()">
+        <div class="material-symbols-outlined">menu</div>
       </a>
 
       <div id="links">
-        <a [routerLink]="['/']">
-          <div class="option">Home</div>
-        </a>
+        <a><div class="option">Home</div></a>
         <a [routerLink]="['/dashboard']"><div class="option">Dashboard</div></a>
         <a [routerLink]="['/profile']"><div class="option">Profile</div></a>
         <a [routerLink]="['/login']"
+          ><div class="option" id="login">Login or Register</div></a
+        >
+      </div>
+      <div id="menu" [@openClose]="openClose()">
+        <a
+          [routerLink]="['/dashboard']"
+          [@openClose]="openClose()"
+          (click)="toggle()"
+          ><div class="option">Dashboard</div></a
+        >
+        <a
+          [routerLink]="['/profile']"
+          [@openClose]="openClose()"
+          (click)="toggle()"
+          ><div class="option">Profile</div></a
+        >
+        <a
+          [routerLink]="['/login']"
+          [@openClose]="openClose()"
+          (click)="toggle()"
           ><div class="option" id="login">Login or Register</div></a
         >
       </div>
@@ -35,5 +78,30 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  title = 'Literacy Link';
+  isOpen = false;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  openClose() {
+    return this.isOpen ? 'open' : 'closed';
+  }
+
+  animationState: string = 'start';
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkWindowSize();
+  }
+
+  ngOnInit() {
+    this.checkWindowSize();
+  }
+
+  private checkWindowSize(): void {
+    if (window.innerWidth > 960) {
+      this.isOpen = false;
+    }
+  }
 }
