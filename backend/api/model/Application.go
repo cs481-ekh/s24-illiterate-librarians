1110ef357f1e1526b6ca04312d805c0490e78463
Andrew Lackey <77242875+ALackey-ai@users.noreply.github.com>
2024-02-24 06:18:12 +0000

    added clarifying notes

--- backend/api/model/Application.go
+++ backend/api/model/Application.go
@@ -4,6 +4,13 @@ import (
 	"gorm.io/gorm"
 )
 
+// NOTE: not all of the things that will be asked in the application will
+// be stored here / in the app_for_tutoring table. Some things like the days
+// that parents will be unavailable will be stored in other tables. For a
+// full list of questions in the application, look at the original applicaiton.
+
+// TODO: list out in the comments here the questions that should be asked for the benefit of future developers
+
 type Application struct {
 	gorm.Model
 	// ParentId  string `json:"parentId"`
--- docs/database_info/database_readme.md
+++ docs/database_info/database_readme.md
@@ -1,3 +1,5 @@
 This folder holds all information related to the design, creation and implementation of the database.
 
 NOTE: the database_schema.drawio can only be opend by the online app "draw.io". This file is the ER diagram for the database and will be used to generate the actual mysql create statements
+
+NOTE: The application and survey tables are technically incomplete representations because of the large amount of questions. For a better understanding of the information stored, simply look at the schema file.
