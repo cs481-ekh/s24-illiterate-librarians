e48f348b2ab845ed0ed74288ade55505a15dd409
cthor <cdthorpe211@gmail.com>
2024-02-25 01:53:50 -0700

    adding login func to dao

--- backend/db/dao.go
+++ backend/db/dao.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"LiteracyLink.com/backend/api/model"
+	"errors"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -8,9 +10,7 @@ import (
 	"os"
 )
 
-var db *gorm.DB
-
-func connectDB() {
+func connectDB() *gorm.DB {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		os.Getenv("Literacy_Link_DB_USERNAME"),
 		os.Getenv("Literacy_Link_DB_PASSWORD"),
@@ -18,14 +18,16 @@ func connectDB() {
 		os.Getenv("Literacy_Link_DB_PORT"),
 		os.Getenv("Literacy_Link_DB_NAME"),
 	)
+	var db *gorm.DB
 	var err error
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
+	return db
 }
 
-func disconnectDB() {
+func disconnectDB(db *gorm.DB) {
 	if db != nil {
 		sqlDB, err := db.DB()
 		if err != nil {
@@ -39,4 +41,15 @@ func disconnectDB() {
 			}
 		}
 	}
+}
+
+func login(request model.LoginRequest) (model.User, error) {
+	db := connectDB()
+	var user model.User
+	result := db.Where("username = ? AND password = ?", request.Username, request.Password).First(user)
+	disconnectDB(db)
+	if result.Error != nil {
+		return user, errors.New("Invalid username or password")
+	}
+	return user, nil
 }
