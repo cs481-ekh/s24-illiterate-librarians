5119fd7ea5b5fe5a91237d6a902a172122ab7750
connorthorpe <cdthorpe211@gmail.com>
2024-03-02 21:36:57 -0700

    working??

--- backend/main.go
+++ backend/main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"LiteracyLink.com/backend/api/routes"
+	"LiteracyLink.com/backend/db"
 	"LiteracyLink.com/backend/middleware"
 	"github.com/gin-gonic/gin"
+	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 )
 
 func main() {
 	// Initialize Gin router
 	router := gin.Default()
-	router.Use(middleware.DatabaseMiddleware())
+	connection := db.ConnectDB()
+	router.Use(middleware.DatabaseMiddleware(connection))
 	routes.SetupRoutes(router)
 	// Set up the routes for the application
 	router.NoRoute(func(c *gin.Context) {
 		c.File(filepath.Join(".", "index.html"))
 	})
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		// Close the database connection during graceful shutdown
+		db.DisconnectDB(connection)
+		os.Exit(0)
+	}()
+
 	// Start the server
 	router.Run(":8080")
 }
--- backend/middleware/middlewareDBConnection.go
+++ backend/middleware/middlewareDBConnection.go
@@ -1,29 +1,20 @@
 package middleware
 
 import (
-	"LiteracyLink.com/backend/db"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 // Database struct to hold the GORM database connection
-type Database struct {
-	Conn *gorm.DB
-}
 
 // Middleware function to set up the GORM database connection
-func DatabaseMiddleware() gin.HandlerFunc {
+func DatabaseMiddleware(conn *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Connect to the database
-		database := db.ConnectDB()
-
 		// Set the GORM database connection into the Gin context
-		c.Set("db", database)
+		c.Set("db", conn)
 
 		// Pass control to the next middleware or route handler
 		c.Next()
 
-		// Disconnect from the database after the request is processed
-		db.DisconnectDB(database)
 	}
 }
