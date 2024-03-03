6cbbfc522f764e431d167a2111b8c33116f63d18
connorthorpe <cdthorpe211@gmail.com>
2024-03-02 22:58:31 -0700

    tweaked to work

--- backend/api/handlers/user/LoginHandler.go
+++ backend/api/handlers/user/LoginHandler.go
@@ -9,17 +9,21 @@ import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
+	"log"
 	"net/http"
 )
 
 func LoginHandler(c *gin.Context) {
 	var request model.LoginRequest
-	err := c.BindJSON(request)
+	err := c.BindJSON(&request)
 	if err != nil {
+		log.Default()
+		log.Println(c.Request.Body)
 		c.JSON(http.StatusBadRequest, gin.H{
 			"status":  "error",
-			"message": fmt.Sprintf("Error while binding login request: %g", err),
+			"message": fmt.Sprintf("Error while binding login request: %s", err),
 		})
+		return
 	}
 
 	dbc := c.MustGet("db").(*gorm.DB)
@@ -58,8 +62,7 @@ func LoginHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"status":  "success",
-		"token":   jwt,
-		"message": fmt.Sprintf("TODO: make func to let user: %s login", request.Username),
+		"status": "success",
+		"token":  jwt,
 	})
 }
