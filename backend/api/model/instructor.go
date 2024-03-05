bbe8f9789e60b3af40d28d3054a184d5ff2e9a4d
Connor Thorpe <97471347+Connor-Thorpe01@users.noreply.github.com>
2024-03-05 13:59:08 -0700

    Delete backend/api/model/instructor.go

--- backend/api/model/instructor.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Instructor struct {
-	InstructorID []byte `gorm:"type:BINARY(16);default:UUID_TO_BIN(UUID(), 1);primaryKey" json:"instructor_id"`
-	UserID       []byte `gorm:"type:BINARY(16);NOT NULL" json:"user_id"`
-
-	// Define a field to represent the relationship with the User model
-	User User `gorm:"foreignKey:UserID"`
-}
