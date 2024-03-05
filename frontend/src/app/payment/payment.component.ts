4eeb8bf2a75e14219d021d482e677243561ac6ec
Xavier Caracter <xaviercaracter@gmail.com>
2024-03-04 19:01:40 -0700

    Reconfiguration of payment file structure

--- frontend/src/app/payment/payment.component.ts
+++ frontend/src/app/payment/payment.component.ts
@@ -2,9 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-payment',
+  template: `
+    <p>
+      payment works!
+    </p>
+  `,
+  styleUrl: './payment.component.css'
 })
 export class PaymentComponent {
   openNewTab() {
     window.open('/payment', 'https://commerce.cashnet.com/boisestateLiteracyLab');
   }
 }
+
+/* import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment',
+})
+export class PaymentComponent {
+  openNewTab() {
+    window.open('/payment', 'https://commerce.cashnet.com/boisestateLiteracyLab');
+  }
+}
+*/
