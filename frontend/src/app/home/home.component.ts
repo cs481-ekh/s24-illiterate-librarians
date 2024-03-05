847bf5aae1cf0a494dc5ccfc976f07d628d8b25d
Ryan Singleton <97577508+ryesing@users.noreply.github.com>
2024-03-05 15:16:59 -0700

    Feature/profile page design (#132)
    * finished home/header
    
    * Made a basic profile/settings page
    
    * the profile stuff i worked on the other day
    
    * Made profile page general template
    
    * Added about page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [],
   templateUrl: 'home.component.html',
   styleUrl: './home.component.css',
 })
