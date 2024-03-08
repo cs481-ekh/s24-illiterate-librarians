28ba428154a57e6c38e2a53875367c12c9949526
Andrew Lackey <77242875+ALackey-ai@users.noreply.github.com>
2024-03-08 00:13:01 +0000

    updated the PostTutorApp handler

--- backend/api/handlers/application/PostTutorApplicationHandler.go
+++ backend/api/handlers/application/PostTutorApplicationHandler.go
@@ -1,17 +1,47 @@
 package application
 
 import (
+	"LiteracyLink.com/backend/api/model"
+	"LiteracyLink.com/backend/db"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 )
 
-//Is a post handler even doing anything if the new application is being sent from the dao function?
 
 func PostTutorApplicationHandler(c *gin.Context) {
-	userId := c.Param("user_id")
-	c.JSON(http.StatusAccepted, gin.H{
-		"status":  "success",
-		"message": fmt.Sprintf("TODO: make func to submit results for tutoring application form user: %s", userId),
-	})
+
+	//Does this attach the application that is passed in with c (ginContext) to the request model 
+	//so that it is submitted to the db correctly?
+	
+	var request model.TutoringApplication
+	err := c.BindJSON(request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "error",
+			"message": fmt.Sprintf("Error while binding tutor application post request: %g", err),
+		})
+	}
+
+	dbc := c.MustGet("db").(*gorm.DB)
+	err = db.SubmitApp(request, dbc)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "failed",
+				"message": fmt.Sprintf("invalid application when trying to post"),
+			})
+			return
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "failed",
+				"message": fmt.Sprintf("ERROR: %g", err),
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK)
 }
