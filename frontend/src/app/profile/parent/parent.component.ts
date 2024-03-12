0f2c0f2e19dc46609fdf2d00e6e602a1b6beeeb9
ryesing <ryansingleton865@u.boisestate.edu>
2024-03-11 22:40:18 -0600

    aestheetic changes to the profile page

--- frontend/src/app/profile/parent/parent.component.ts
+++ frontend/src/app/profile/parent/parent.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './parent.component.css'
 })
 export class ProfileParentComponent {
+  ngOnInit() {
+    const editButton = document.getElementById('edit');
+    const saveButton = document.getElementById('save');
+    const profileEditing = document.getElementById('profile-editing');
+    const profileView = document.getElementById('profile-view');
+    
 
+    if (editButton && saveButton && profileEditing && profileView) {
+      editButton.addEventListener('click', function () {
+        editButton.style.display = 'none';
+        profileEditing.style.display = 'block';
+        saveButton.style.display = 'block';
+        profileView.style.display = 'none';
+      });
+
+      saveButton.addEventListener('click', function () {
+        editButton.style.display = 'block';
+        profileEditing.style.display = 'none';
+        saveButton.style.display = 'none';
+        profileView.style.display = 'block';
+      });
+    }
+  }
 }
