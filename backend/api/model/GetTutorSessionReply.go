472922e989bcde1927d1ce9bbf8d711aef9a4914
connorthorpe <cdthorpe211@gmail.com>
2024-03-16 22:28:16 -0600

    done???

--- backend/api/model/GetTutorSessionReply.go
+++ backend/api/model/GetTutorSessionReply.go
@@ -8,7 +8,4 @@ type GetTutorSessionReply struct {
 	ZoomRecordingLink string    `json:"zoom_recording_link"`
 	MeetingDate       time.Time `json:"meeting_date"`
 	ParentAvail       bool      `json:"parent_avail"`
-	SemesterType      string    `json:"semester_type"`
-	SemesterStart     time.Time `json:"semester_start"`
-	SemesterEnd       time.Time `json:"semester_end"`
 }
